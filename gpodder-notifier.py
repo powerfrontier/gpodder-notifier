d93e55a292e12bd4781d83674dedb78343d99261
frontier <power_frontier@yahoo.es>
2022-02-07 19:41:17 +0100

    Fix: get user home path to access gpodder config

--- gpodder-notifier.py
+++ gpodder-notifier.py
@@ -7,20 +7,26 @@
 # dependencias: notify-send.sh -> https://aur.archlinux.org/packages/notify-send.sh
 # gpodder -> https://archlinux.org/packages/community/any/gpodder/
 
+#TODO hacer que se procesen argumentos de entrada y que algunos sean:
+# --verbose: poner algunas trazas de debug
+# --no-update: no hace el gpo update, solo el gpo pending
+# --gpodder-home: indica el lugar de instalación de gpodder del usuario (para acceder a la db)
+
+
 import subprocess
 encoding = "utf-8"
 
-def process_text(text_update, text_pending):
-	# Debug strings for not invocate gpodder cli on every test
-	'''
-	text_update = "Checking for new episodes\n\
+
+# Debug strings for not invocate gpodder cli on every test
+#	'''
+text_update = "Checking for new episodes\n\
  TED Talks Daily (SD video)                                              [FAIL]\n\
  Linux Outlaws                                                           [SKIP]\n\
- Radiolab from WNYC                                                      [DONE]\n\
+ Radiolab from WNYC                                                      [FAIL]\n\
 21 new episodes\n"
-	'''
-	'''
-	text_pending = "# TED Talks Daily (SD video)\n\
+#	'''
+#	'''
+text_pending = "# TED Talks Daily (SD video)\n\
   How couples can sustain a strong sexual connection for a lifetime | Emily Nagoski\n\
   Can seaweed help curb global warming? | Tim Flannery\n\
   A bold plan to empower 1.6 million out-of-school girls in India | Safeena Husain\n\
@@ -44,7 +50,11 @@ def process_text(text_update, text_pending):
   Linux Outlaws 363 – OggCamp 14 Podcaster Panel\n\
   Linux Outlaws 362 – Hitler Funk\n\
 21 new episodes\n"
-	'''
+#	'''
+
+
+
+def process_text(text_update, text_pending):
 	msg = "\n"
 	lines_update = text_update.splitlines()
 
@@ -52,6 +62,8 @@ def process_text(text_update, text_pending):
 	nDone = 0
 	nSkip = 0
 	nFail = 0
+	lineasFail = [0] * len(lines_update) # initialize the vector with 0s
+
 	for i in range(1, len(lines_update)-1) :
 		if lines_update[i].endswith("[DONE]") :
 			nDone += 1
@@ -59,15 +71,22 @@ def process_text(text_update, text_pending):
 			nSkip += 1
 		elif lines_update[i].endswith("[FAIL]") :
 			nFail += 1
+			lineasFail[i] = 1
 		else :
 			msg = msg + "WTF!!!!: " + lines_update[i] + "\n"
+
 	msg = msg + "[DONE]: " + str(nDone) + "\n"
 	msg = msg + "[SKIP]: " + str(nSkip) + "\n"
+
 	if nFail > 0 :
-		msg = msg + "<b>" + "[FAIL]: " + str(nFail) + "</b>" + "\n\n"
-	else:
-		msg = msg + "[FAIL]: " + str(nFail) + "\n\n"
-		
+		msg = msg + "<b>" + "[FAIL]: " + str(nFail) + "</b>" + "\n"
+		for i in range(len(lineasFail)) :
+			if lineasFail[i] == 1 :
+				msg = msg + "    " + lines_update[i].split("[")[0].strip() + "\n"
+		msg = msg + "\n"
+	else :
+		msg = msg + "[FAIL]: 0\n\n"
+
 	if text_pending[0] == "#" :
 		lines_pending = text_pending.split("\n# ")
 		podcasts_episodes = []
@@ -90,7 +109,7 @@ def process_text(text_update, text_pending):
 		number_new_episodes = text_pending
 
 	msg = msg + "\n<b>" + number_new_episodes + "</b>"
-	#print(msg)
+	print(msg)
 
 	return msg
 
@@ -122,8 +141,8 @@ LIST_SYMBOL = '#'
 gpo_command_update = "update"
 gpo_command_pending = "pending"
 notify_send_command = "notify-send.sh"
-text_update = ""
-text_pending= ""
+#text_update = ""
+#text_pending= ""
 text_update = subprocess.check_output(["gpo", gpo_command_update]).decode(encoding)
 text_pending = subprocess.check_output(["gpo", gpo_command_pending]).decode(encoding)
 
@@ -136,12 +155,8 @@ action_open = "--action=Open gPodder:gpodder"
 action_default = "--default-action=gpodder"
 
 if text_pending[0] == "#" :
-	action_mark_old = "--action=Mark as old:python /media/Datos/GDrive/check-gpodder/mark-gpodder.py"
+	action_mark_old = "--action=Mark as old:python /media/Datos/Proyectos/gpodder-notifier/mark-gpodder.py"
 	action_download = "--action=Download all:gpo download && notify-send 'Podcasts downloads:' 'Finished'"
 	subprocess.call([notify_send_command, icon, action_open, action_mark_old, action_download, action_default, head, msg])
 else: # Case of no pending episodes text_pending="0 episodios nuevos"
 	subprocess.call([notify_send_command, icon, action_open, action_default, head, msg])
-
-#subprocess.call(['./notify-send.sh', icon, action_open, action_download, action_default, head, msg])
-#subprocess.call(['/media/Datos/GDrive/check-gpodder/notify-send.sh', icon, action_open, action_mark_old, action_download, action_default, head, msg])
-
