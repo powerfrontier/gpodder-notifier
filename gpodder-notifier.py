59228f77751710ca586bcd73861951523482d2de
frontier <power_frontier@yahoo.es>
2022-07-20 02:22:40 +0200

    cooldown and max podcasts update options

--- gpodder-notifier.py
+++ gpodder-notifier.py
@@ -9,12 +9,51 @@
 
 #TODO hacer que se procesen argumentos de entrada y que algunos sean:
 # --verbose: poner algunas trazas de debug
-# --no-update: no hace el gpo update, solo el gpo pending
+# --no-update: no hace el gpo update, solo el gpo pending <- ir al github del gpodder y buscar una request de esta feature y sino está perdirla yo
 # --gpodder-home: indica el lugar de instalación de gpodder del usuario (para acceder a la db)
+
+# TODO: hacer fichero de configuracion con opciones por defecto y que se parsee al inicio (hacer este antes del otro)
+
+
+
+# TODO: si no hay conexión a internet la lista de podcasts que fallan puede ser muy larga -> poner un máximo x de podcasts en la lista que fallan
 
 
 import subprocess
+import datetime
+import configargparse
+import Defaults
+
+# parsing arguments and config file
+parser = configargparse.ArgParser(default_config_files=['./gpodder-notifier.conf'])
+
+parser.add('-ct', '--cooldown-time', default=Defaults.COOLDOWN_TIME, help='cooldown time in minutes: minimun time beetween updates')  # this option can be set in a config file because it starts with '--'
+parser.add('-c', '--my-config', is_config_file=True, help='config file path')
+parser.add('-v', help='verbose', action='store_true')
+
+options = parser.parse_args()
+
+#print(options)
+#print("----------")
+#print(parser.format_help())
+#print("----------")
+#print(parser.format_values())    # useful for logging where different settings came from
+#print("----------")
+
 encoding = "utf-8"
+
+cooldown = datetime.timedelta(minutes=int(options.cooldown_time))
+
+# lastcheck recovery
+f = open("gpodder-notifier.dat", "r")
+line = f.readline()
+f.close()
+
+#lastcheck = datetime.datetime(2022, 7, 12, 21, 48)
+lastcheck = datetime.datetime.fromisoformat(line.strip()) #strip is for get rid of the trailing /n character of the return of readline() function
+now = datetime.datetime.now()
+diff = now - lastcheck
+
 
 
 # Debug strings for not invocate gpodder cli on every test
@@ -97,13 +136,20 @@ def process_text(text_update, text_pending):
 		podcasts_episodes[0][0] = podcasts_episodes[0][0].replace("# ", "") # Preparation of the first podcast title because it's different from the others
 		number_new_episodes = podcasts_episodes[-1].pop(-1) # Exclude last line of because it's not an episode but the number of total pending episodes and remember it for later use
 
+		nPodcasts = len(podcasts_episodes)
 
+		if len(podcasts_episodes) > int(Defaults.MAX_PODCASTS) :
+			nPodcasts = int(Defaults.MAX_PODCASTS)
+			print(nPodcasts)
 
-		for i in range(len(podcasts_episodes)):
+		for i in range(nPodcasts):
 			podcast_name = podcasts_episodes[i][0] #.lstrip().replace("\n", "")
 			msg = msg + LIST_SYMBOL + ' ' + podcast_name + " <b>(" + str(len(podcasts_episodes[i])-1) + ")</b>" + "\n"
 			msg = msg + pending_episodes(podcasts_episodes[i][1:]) # No pasamos el primer elemento (el [0] del vector) porque es el título del podcast
 			msg = msg + "\n"
+
+		if len(podcasts_episodes) > int(Defaults.MAX_PODCASTS) :
+			msg = msg + "....\n....\nand more\n"
 
 	else: # Case of no pending episodes text_pending="0 episodios nuevos"
 		number_new_episodes = text_pending
@@ -143,21 +189,36 @@ gpo_command_pending = "pending"
 notify_send_command = "notify-send.sh"
 #text_update = ""
 #text_pending= ""
-text_update = subprocess.check_output(["gpo", gpo_command_update]).decode(encoding)
-text_pending = subprocess.check_output(["gpo", gpo_command_pending]).decode(encoding)
-
-msg =  process_text(text_update, text_pending)
+#text_update = subprocess.check_output(["gpo", gpo_command_update]).decode(encoding)
+#text_pending = subprocess.check_output(["gpo", gpo_command_pending]).decode(encoding)
 
 
-head = "Podcasts updates:"
-icon = "--icon=gpodder"
-action_open = "--action=Open gPodder:gpodder"
-action_default = "--default-action=gpodder"
+if diff > cooldown :
+	print("HA pasado el tiempo de cooldown")
+	lastcheck = now
+	# save lastcheck
+	f = open("gpodder-notifier.dat", "w")
+	f.write(now.isoformat())
+	f.close()
 
-if text_pending[0] == "#" :
-	action_mark_old = "--action=Mark as old:python ./mark-gpodder.py"
-	# debug action_mark_old = "--action=Mark as old:zenity --info --title 'Test' --text \"$PWD\""
-	action_download = "--action=Download all:gpo download && notify-send 'Podcasts downloads:' 'Finished'"
-	subprocess.call([notify_send_command, icon, action_open, action_mark_old, action_download, action_default, head, msg])
-else: # Case of no pending episodes text_pending="0 episodios nuevos"
-	subprocess.call([notify_send_command, icon, action_open, action_default, head, msg])
+
+
+	msg =  process_text(text_update, text_pending)
+
+
+	head = "Podcasts updates:"
+	icon = "--icon=gpodder"
+	action_open = "--action=Open gPodder:gpodder"
+	action_default = "--default-action=gpodder"
+
+	if text_pending[0] == "#" :
+		action_mark_old = "--action=Mark as old:python ./mark-gpodder.py"
+		# debug action_mark_old = "--action=Mark as old:zenity --info --title 'Test' --text \"$PWD\""
+		action_download = "--action=Download all:gpo download && notify-send 'Podcasts downloads:' 'Finished'"
+	#	subprocess.call([notify_send_command, icon, action_open, action_mark_old, action_download, action_default, head, msg])
+	else: # Case of no pending episodes text_pending="0 episodios nuevos"
+	#	subprocess.call([notify_send_command, icon, action_open, action_default, head, msg])
+		print("end")
+else :
+	print("no ha pasado el tiempo de cooldown")
+	#print(diff)
