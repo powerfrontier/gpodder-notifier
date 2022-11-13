bbb3c027b62d74d21b1e0f18891b1c661767a436
frontier <power_frontier@yahoo.es>
2022-11-13 15:53:32 +0100

    fixing up things

--- gpodder-notifier.py
+++ gpodder-notifier.py
@@ -17,6 +17,100 @@ import os
 
 import Defaults
 
+
+def process_text(text_update, text_pending):
+	msg = "\n"
+	if text_update != "" :
+		lines_update = text_update.splitlines()
+
+		msg = msg + lines_update[0] + ":\n"
+		nDone = 0
+		nSkip = 0
+		nFail = 0
+		lineasFail = [0] * len(lines_update) # initialize the vector with 0s
+
+		for i in range(1, len(lines_update)-1) :
+			if lines_update[i].endswith("[DONE]") :
+				nDone += 1
+			elif lines_update[i].endswith("[SKIP]") :
+				nSkip += 1
+			elif lines_update[i].endswith("[FAIL]") :
+				nFail += 1
+				lineasFail[i] = 1
+			else :
+				msg = msg + "WTF!!!!: " + lines_update[i] + "\n"
+
+		msg = msg + "[DONE]: " + str(nDone) + "\n"
+		msg = msg + "[SKIP]: " + str(nSkip) + "\n"
+
+		if nFail > 0 :
+			msg = msg + "<b>" + "[FAIL]: " + str(nFail) + "</b>" + "\n"
+			j = 0
+			for i in range(len(lineasFail)) :
+				if j <= Defaults.MAX_FAIL_PODCASTS :
+					if lineasFail[i] == 1 :
+						msg = msg + "    " + lines_update[i].split("[")[0].strip() + "\n"
+						j +=  1
+			if nFail >= Defaults.MAX_FAIL_PODCASTS :
+				msg = "....\n....\nand more podcasts that failed"
+			msg = msg + "\n"
+		else :
+			msg = msg + "[FAIL]: 0\n\n"
+
+	if text_pending[0] == "#" :
+		lines_pending = text_pending.split("\n# ")
+		podcasts_episodes = []
+
+		for i in lines_pending:
+			podcasts_episodes.append(i.splitlines())
+		#print(podcasts_episodes)
+		podcasts_episodes[0][0] = podcasts_episodes[0][0].replace("# ", "") # Preparation of the first podcast title because it's different from the others
+		number_new_episodes = podcasts_episodes[-1].pop(-1) # Exclude last line of because it's not an episode but the number of total pending episodes and remember it for later use
+
+		nPodcasts = len(podcasts_episodes)
+
+		if len(podcasts_episodes) > int(Defaults.MAX_PODCASTS) :
+			nPodcasts = int(Defaults.MAX_PODCASTS)
+			print(nPodcasts)
+
+		for i in range(nPodcasts):
+			podcast_name = podcasts_episodes[i][0] #.lstrip().replace("\n", "")
+			msg = msg + Defaults.LIST_SYMBOL + ' ' + podcast_name + " <b>(" + str(len(podcasts_episodes[i])-1) + ")</b>" + "\n"
+			msg = msg + pending_episodes(podcasts_episodes[i][1:]) # No pasamos el primer elemento (el [0] del vector) porque es el título del podcast
+			msg = msg + "\n"
+
+		if len(podcasts_episodes) > int(Defaults.MAX_PODCASTS) :
+			msg = msg + "....\n....\nand more podcasts with new episodes\n"
+
+	else: # Case of no pending episodes text_pending="0 episodios nuevos"
+		number_new_episodes = text_pending
+
+	msg = msg + "\n<b>" + number_new_episodes + "</b>"
+
+	return msg
+
+
+def pending_episodes(episodes):
+	#print(episodes)
+	sEpisodes = ""
+	if len(episodes) <= Defaults.MAX_EPISODES_PER_PODCAST :
+		numberEpisodes = len(episodes)
+	else :
+		numberEpisodes = Defaults.MAX_EPISODES_PER_PODCAST
+
+	for i in range(numberEpisodes):
+		episode = episodes[i]
+		if len(episode) > Defaults.MAX_LENGTH :
+			episode = episode[0:Defaults.MAX_LENGTH-3] + "..."
+		sEpisodes = sEpisodes + "  " + "<i>" + episode + "</i>" + "\n"
+
+	if len(episodes) > Defaults.MAX_EPISODES_PER_PODCAST :
+		sEpisodes = sEpisodes + "    ..."
+
+	return sEpisodes
+
+
+
 # parsing arguments and config file
 parser = configargparse.ArgParser(default_config_files=['./gpodder-notifier.conf'])
 
@@ -29,47 +123,51 @@ parser.add('-v', help='verbose', action='store_true')
 options = parser.parse_args()
 
 if options.v == True :
-	#print(options)
-	print("----------")
-	#print(parser.format_help())
-	print("----------")
 	print(parser.format_values())    # useful for logging where different settings came from
-	print("----------")
 
 encoding = "utf-8"
 cooldown = datetime.timedelta(minutes=int(options.cooldown_time))
 USER_PATH = os.path.expanduser('~')
 
 # lastcheck recovery
-#print(USER_PATH + Defaults.GPODDER_NOTIFIER_USER_PATH + Defaults.GPODDER_NOTIFIER_DAT_NAME)
+gpodder_notifier_dat_path = USER_PATH + Defaults.GPODDER_NOTIFIER_USER_PATH + Defaults.GPODDER_NOTIFIER_DAT_NAME
+if options.v == True :
+	print("----------")
+	print("COOLDOWN TIME CALCULATIONS")
+	print("gpodder notifier dat path: " + gpodder_notifier_dat_path)
 try:
-	f = open(USER_PATH + Defaults.GPODDER_NOTIFIER_USER_PATH + Defaults.GPODDER_NOTIFIER_DAT_NAME, "r")
+	f = open(gpodder_notifier_dat_path, "r")
 except FileNotFoundError:
-	#print("file not found!!")
-	f = open(USER_PATH + Defaults.GPODDER_NOTIFIER_USER_PATH + Defaults.GPODDER_NOTIFIER_DAT_NAME, "a+")
+	if options.v == True :
+		print("gpodder notifier dat file not found! Creating one.")
+	f = open(gpodder_notifier_dat_path, "a+")
 	f.write("2018-11-06 10:20:23.283") # some random past date
 	f.seek(0)
 finally:
 	line = f.readline()
-	#print(line)
+	if options.v == True :	
+		print("Last check: " + line)
 	f.close()
 
 #lastcheck = datetime.datetime(2022, 7, 12, 21, 48)
 lastcheck = datetime.datetime.fromisoformat(line.strip()) #strip is for get rid of the trailing /n character of the return of readline() function
 now = datetime.datetime.now()
 diff = now - lastcheck
-
+if options.v == True :
+	print("Now is: " + now.isoformat())
+	print("Difference is: " + str(diff))
+	print("Cooldown time is: " + str(cooldown))
 
 
 # Debug strings for not invocate gpodder cli on every test
-#	'''
+	'''
 text_update = "Checking for new episodes\n\
  TED Talks Daily (SD video)                                              [FAIL]\n\
  Linux Outlaws                                                           [SKIP]\n\
  Radiolab from WNYC                                                      [FAIL]\n\
 21 new episodes\n"
-#	'''
-#	'''
+	'''
+	'''
 text_pending = "# TED Talks Daily (SD video)\n\
   How couples can sustain a strong sexual connection for a lifetime | Emily Nagoski\n\
   Can seaweed help curb global warming? | Tim Flannery\n\
@@ -94,125 +192,32 @@ text_pending = "# TED Talks Daily (SD video)\n\
   Linux Outlaws 363 – OggCamp 14 Podcaster Panel\n\
   Linux Outlaws 362 – Hitler Funk\n\
 21 new episodes\n"
-#	'''
+	'''
 
 
 
-def process_text(text_update, text_pending):
-	msg = "\n"
-	lines_update = text_update.splitlines()
-
-	msg = msg + lines_update[0] + ":\n"
-	nDone = 0
-	nSkip = 0
-	nFail = 0
-	lineasFail = [0] * len(lines_update) # initialize the vector with 0s
-
-	for i in range(1, len(lines_update)-1) :
-		if lines_update[i].endswith("[DONE]") :
-			nDone += 1
-		elif lines_update[i].endswith("[SKIP]") :
-			nSkip += 1
-		elif lines_update[i].endswith("[FAIL]") :
-			nFail += 1
-			lineasFail[i] = 1
-		else :
-			msg = msg + "WTF!!!!: " + lines_update[i] + "\n"
-
-	msg = msg + "[DONE]: " + str(nDone) + "\n"
-	msg = msg + "[SKIP]: " + str(nSkip) + "\n"
-
-	if nFail > 0 :
-		msg = msg + "<b>" + "[FAIL]: " + str(nFail) + "</b>" + "\n"
-		j = 0
-		for i in range(len(lineasFail)) :
-			if j <= Defaults.MAX_FAIL_PODCASTS :
-				if lineasFail[i] == 1 :
-					msg = msg + "    " + lines_update[i].split("[")[0].strip() + "\n"
-					j +=  1
-		if nFail >= Defaults.MAX_FAIL_PODCASTS :
-			msg = "....\n....\nand more podcasts that failed"
-		msg = msg + "\n"
-	else :
-		msg = msg + "[FAIL]: 0\n\n"
-
-	if text_pending[0] == "#" :
-		lines_pending = text_pending.split("\n# ")
-		podcasts_episodes = []
-
-		for i in lines_pending:
-			podcasts_episodes.append(i.splitlines())
-		#print(podcasts_episodes)
-		podcasts_episodes[0][0] = podcasts_episodes[0][0].replace("# ", "") # Preparation of the first podcast title because it's different from the others
-		number_new_episodes = podcasts_episodes[-1].pop(-1) # Exclude last line of because it's not an episode but the number of total pending episodes and remember it for later use
-
-		nPodcasts = len(podcasts_episodes)
-
-		if len(podcasts_episodes) > int(Defaults.MAX_PODCASTS) :
-			nPodcasts = int(Defaults.MAX_PODCASTS)
-			print(nPodcasts)
-
-		for i in range(nPodcasts):
-			podcast_name = podcasts_episodes[i][0] #.lstrip().replace("\n", "")
-			msg = msg + LIST_SYMBOL + ' ' + podcast_name + " <b>(" + str(len(podcasts_episodes[i])-1) + ")</b>" + "\n"
-			msg = msg + pending_episodes(podcasts_episodes[i][1:]) # No pasamos el primer elemento (el [0] del vector) porque es el título del podcast
-			msg = msg + "\n"
-
-		if len(podcasts_episodes) > int(Defaults.MAX_PODCASTS) :
-			msg = msg + "....\n....\nand more podcasts with new episodes\n"
-
-	else: # Case of no pending episodes text_pending="0 episodios nuevos"
-		number_new_episodes = text_pending
-
-	msg = msg + "\n<b>" + number_new_episodes + "</b>"
-	print(msg)
-
-	return msg
-
-
-def pending_episodes(episodes):
-	#print(episodes)
-	sEpisodes = ""
-	if len(episodes) <= MAX_EPISODES_PER_PODCAST :
-		numberEpisodes = len(episodes)
-	else :
-		numberEpisodes = MAX_EPISODES_PER_PODCAST
-
-	for i in range(numberEpisodes):
-		episode = episodes[i]
-		if len(episode) > MAX_LENGTH :
-			episode = episode[0:MAX_LENGTH-3] + "..."
-		sEpisodes = sEpisodes + "  " + "<i>" + episode + "</i>" + "\n"
-
-	if len(episodes) > MAX_EPISODES_PER_PODCAST :
-		sEpisodes = sEpisodes + "    ..."
-
-	return sEpisodes
-
-# Constats and ¿arguments?
-MAX_LENGTH = 60
-MAX_EPISODES_PER_PODCAST = 5
-LIST_SYMBOL = '#'
-
-gpo_command_update = "update"
-gpo_command_pending = "pending"
-notify_send_command = "notify-send.sh"
-#text_update = ""
-#text_pending= ""
 
 if diff > cooldown :
-	print("HA pasado el tiempo de cooldown")
+	if options.v == True :
+		print("Cooldown time has passed.")
 	lastcheck = now
 	# save lastcheck
 	f = open(USER_PATH + Defaults.GPODDER_NOTIFIER_USER_PATH + Defaults.GPODDER_NOTIFIER_DAT_NAME, "w")
 	f.write(now.isoformat())
 	f.close()
-
-	#text_update = subprocess.check_output(["gpo", gpo_command_update]).decode(encoding)
-	#text_pending = subprocess.check_output(["gpo", gpo_command_pending]).decode(encoding)
+	if options.v == True :
+		print("Writting new last check on gpodder notifier dat file: " + now.isoformat())
+	text_update = ""
+	if options.no_update == False :
+		text_update = subprocess.check_output([Defaults.GPO_COMMAND, Defaults.GPO_COMMAND_UPDATE]).decode(encoding)
+	text_pending = subprocess.check_output([Defaults.GPO_COMMAND, Defaults.GPO_COMMAND_PENDING]).decode(encoding)
 
 	msg =  process_text(text_update, text_pending)
-
+	
+	if options.v == True :
+		print("----------")
+		print("PROCESS OF NEW EPISODES")
+		print(msg)
 
 	head = "Podcasts updates:"
 	icon = "--icon=gpodder"
@@ -221,13 +226,13 @@ if diff > cooldown :
 
 	if text_pending[0] == "#" :
 		action_mark_old = "--action=Mark as old:python "+Defaults.GPODDER_NOTIFIER_PATH+"mark-gpodder.py"
-		#print(action_mark_old)
 		# debug action_mark_old = "--action=Mark as old:zenity --info --title 'Test' --text \"$PWD\""
 		action_download = "--action=Download all:gpo download && notify-send 'Podcasts downloads:' 'Finished'"
-		subprocess.call([notify_send_command, icon, action_open, action_mark_old, action_download, action_default, head, msg])
+		subprocess.call([Defaults.NOTIFY_SEND_COMMAND, icon, action_open, action_mark_old, action_download, action_default, head, msg])
 	else: # Case of no pending episodes text_pending="0 episodios nuevos"
-		subprocess.call([notify_send_command, icon, action_open, action_default, head, msg])
-		print("end")
+		subprocess.call([Defaults.NOTIFY_SEND_COMMAND, icon, action_open, action_default, head, msg])
+		if options.v == True :
+			print("Case of no pending episodes")
 else :
-	print("no ha pasado el tiempo de cooldown")
-	#print(diff)
+	if options.v == True :
+		print("Cooldown time has NO passed.")
